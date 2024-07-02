68d861b9824874ffd830df5bdfe34a2ef4fdfba2
faisalahammed044274 <faisalahammed044274@gmail.com>
2024-07-02 16:47:51 +0600

    Array and loops

---
+++ test/03.array.php
@@ -0,0 +1,15 @@
+<?php
+
+// Array
+// 1. Indexed array
+// 2. Associative array
+// 3. Multidimensional array
+
+$fruits = array('Mango','Orange', 'Banana', 'Apple');
+
+echo $fruits[3];
+echo $fruits[1];
+echo $fruits[0];
+var_dump($fruits);
+echo '</br>'; 
+echo $fruits();
---
+++ test/04.loops.php
@@ -0,0 +1,12 @@
+<?php
+
+$cars = ["Volvo", "BMW", "Toyota"];
+
+$count = 0;
+while($count <count($cars)){
+    echo $cars[$count];
+    echo PHP_EOL;
+    // $count = $count +1;
+    $count++;
+
+}
