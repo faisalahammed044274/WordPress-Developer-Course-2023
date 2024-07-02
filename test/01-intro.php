40378a39f99daf4e11414079a8af5084a8327324
faisalahammed044274 <faisalahammed044274@gmail.com>
2024-07-02 16:28:25 +0600

    test

---
+++ test/01-intro.php
@@ -0,0 +1,30 @@
+<?php
+
+echo "Hello";
+echo PHP_EOL;
+
+$x = 97;
+$y = 3;
+
+$z = $x + $y;
+$a = ($z -$x) + $z;
+echo $z;
+echo PHP_EOL;
+echo $a;
+
+// Data types :
+
+// 1. Integer
+$gh = 325;
+// 2. Float
+$gtt = 3.25;
+// 3. String
+$g = "Hello";
+// 4. Boolean
+$isLoggedIn = True;
+// 5. Array
+$fruits = array("Apple", "Banana", "Orange");
+// 6. Object
+$person = new Person("John", "Doe");
+// 7. Null
+$null = null;
---
+++ test/02.functions.php
@@ -0,0 +1,11 @@
+<?php
+
+function greeting($msg) {
+    echo $msg;
+}
+
+greeting("Hey there");
+echo PHP_EOL;
+greeting("oiii ko jas");
+echo PHP_EOL;
+greeting("kiree kamon asos");
