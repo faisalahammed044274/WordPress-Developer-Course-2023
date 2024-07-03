292e9a30c40c9db358ca1f933870787a4731c1ce
faisalahammed044274 <faisalahammed044274@gmail.com>
2024-07-03 11:58:54 +0600

    template

---
+++ wp-content/themes/udemy/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php  language_attributes(); ?> >
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <?php wp_head(); ?>
+
+</head>
+<body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+    <h2>Hello World</h2>
+
+<?php wp_footer(); ?>    
+</body>
+</html>
